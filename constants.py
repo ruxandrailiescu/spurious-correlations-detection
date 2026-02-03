2221d0719a50322b9fe28cdb86b087f30cd2bb57
ruxandrailiescu <iliescuruxandra23@stud.ase.ro>
2026-02-03 22:23:18 +0200

    adding visualization scripts for pca and umap

--- constants.py
+++ constants.py
@@ -9,20 +9,8 @@ DATASET_FILE = {
     'mnli': 'input_mnli.csv'
 }
 
-METADATA_FILE = {
-    'mnli': 'metadata_mnli.csv'
-}
-
-DATASET_SPLITS = {
-    'train': 0,
-    'val': 1,
-    'test': 2,
-}
-
 DATASET_CLASSES = {
     'mnli': ['fiction', 'government', 'slate', 'telephone', 'travel']
 }
 
 SEED = 5454
-
-SPLITS = ['train', 'val']
